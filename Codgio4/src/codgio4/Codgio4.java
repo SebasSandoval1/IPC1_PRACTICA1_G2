bf1e3cf3ee06056128a14b58b0a456c28653df96
Sergio Sandoval <sebasruiz925@gmail.com>
2021-06-05 16:06:36 -0600

    ArchivoOficial

--- Codgio4/src/codgio4/Codgio4.java
+++ Codgio4/src/codgio4/Codgio4.java
@@ -1,17 +1,15 @@
-
-
-package codigooperacionesmatricez;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codgio4;
 
 import java.io.*;
 import java.util.Scanner;
+public class Codgio4 {
 
-public class CodigoOperacionesMatricez {
-    
-    //------------------------------------------------
-
-    //para leer el archivo de entrada
-
-    //------------------------------------------------
+   //------------------------------------------------
 
     //para leer el archivo de entrada
 
