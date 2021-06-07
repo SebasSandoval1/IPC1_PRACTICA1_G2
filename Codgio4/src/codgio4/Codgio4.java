0b31a93a9936e8afc56c16376b6b0458e7642b95
André <cardenasvale04@gmail.com>
2021-06-06 20:12:18 -0600

    agregado parte de multiplicacion

--- Codgio4/src/codgio4/Codgio4.java
+++ Codgio4/src/codgio4/Codgio4.java
@@ -321,6 +321,7 @@ public class Codgio4 {
                     
                     //ingreso de la segunda matriz a operar
 
+                    System.out.println("Ingrese la letra de la segunda matriz: ");
                     opm1 = op1.nextLine();
 
                     switch (opm1) {
@@ -511,7 +512,269 @@ public class Codgio4 {
 
                     case 3: {
 
-                        System.out.println("Has seleccionado la opcion 3");
+                        System.out.println("Has seleccionado la opcion 3: Multiplicación");
+                        
+                        //submenú de la opción multiplicacion de matrices
+
+                    System.out.println("============MENU MULTIPLICACIÓN============");
+                    System.out.println("1. Multiplicar matriz por un número");
+                    System.out.println("2. Multiplicar matriz por otra matriz");
+                    System.out.println("===========================================");
+
+                    Scanner numeropedido = new Scanner(System.in);
+                    int numero; //Opcion que debe elegir el usuario
+                    System.out.println("¿Qué operación desea realizar?: ");
+                    numero = numeropedido.nextInt();
+
+                    //--------------------------------------------------------------
+                    
+                     switch (numero) {
+
+                        case 1: {
+
+                            System.out.println("Has seleccionado la opción 1: Matriz por un número");
+
+                            Scanner teclado2 = new Scanner(System.in);
+                            int indicenumero; //Opcion que debe elegir el usuario
+                            System.out.println("¿Qué operación desea realizar?: ");
+                            indicenumero = teclado2.nextInt();
+
+
+
+                            break;
+
+                        }
+
+                        case 2: {
+
+                            System.out.println("Has seleccionado la opción 2: Matriz por otra matriz");
+
+                            //multiplicacion de dos matrices propuestas por el usuario
+
+                            int[][] m5 = new int[3][3];
+                            m5[0][0] = 1;
+                            m5[0][1] = 2;
+                            m5[0][2] = 3;
+                            m5[1][0] = 4;
+                            m5[1][1] = 5;
+                            m5[1][2] = 6;
+                            m5[2][0] = 7;
+                            m5[2][1] = 8;
+                            m5[2][2] = 9;
+
+                            int[][] m6 = new int[3][3];
+                            m6[0][0] = 9;
+                            m6[0][1] = 6;
+                            m6[0][2] = 8;
+                            m6[1][0] = 1;
+                            m6[1][1] = 4;
+                            m6[1][2] = 7;
+                            m6[2][0] = 2;
+                            m6[2][1] = 3;
+                            m6[2][2] = 5;
+
+                            int[][] matResultantes = new int[m5.length][m6[0].length];
+                            int[][] primerTerR = new int[1][1];
+                            int[][] segundoTerR = new int[1][1];
+                            int[][] tercerTerR = new int[1][1];
+                            int[][] cuartoTerR = new int[1][1];
+                            int[][] quintoTerR = new int[1][1];
+                            int[][] sextoTerR = new int[1][1];
+                            int[][] septTerR = new int[1][1];
+                            int[][] octaTerR = new int[1][1];
+                            int[][] noveTerR = new int[1][1];
+
+                            //selección de las matrices a operar
+
+                            //---------------------------------------------
+
+
+
+                            //---------------------------------------------
+
+
+                            //creación de la matriz resultante y luego impresa en la consola
+
+                            //---------------------------------------------
+                            
+                            //creación del primer termino de la matriz resultante
+
+                            for (int i = 0; i < 1; i++) {
+                                for (int j = 0; j < 1; j++) {
+                                    primerTerR[i][j] = (m5[0][0]*m6[0][0])+(m5[0][1]*m6[1][0])+(m5[0][2]*m6[2][0]);
+                                }
+                            }
+
+                            //creacion del segundo termino de la matriz resultante
+
+                            for (int i = 0; i < 1; i++) {
+                                for (int j = 0; j < 1; j++) {
+                                    segundoTerR[i][j] = (m5[0][0]*m6[0][1])+(m5[0][1]*m6[1][1])+(m5[0][2]*m6[2][1]);
+                                }
+                            }
+
+                            //creacion del tercer termino de la matriz resultante
+
+                            for (int i = 0; i < 1; i++) {
+                                for (int j = 0; j < 1; j++) {
+                                    tercerTerR[i][j] = (m5[0][0]*m6[0][2])+(m5[0][1]*m6[1][2])+(m5[0][2]*m6[2][2]);
+                                }
+                            }
+
+                            //creación del cuarto termino de la matriz resultante
+
+                            for (int i = 0; i < 1; i++) {
+                                for (int j = 0; j < 1; j++) {
+                                    cuartoTerR[i][j] = (m5[1][0]*m6[0][0])+(m5[1][1]*m6[1][0])+(m5[1][2]*m6[2][0]);
+                                }
+                            }
+
+                            //creación del quinto termino de la matriz resultante
+
+                            for (int i = 0; i < 1; i++) {
+                                for (int j = 0; j < 1; j++) {
+                                    quintoTerR[i][j] = (m5[1][0]*m6[0][1])+(m5[1][1]*m6[1][1])+(m5[1][2]*m6[2][1]);
+                                }
+                            }
+
+                            //creación del sexto termino de la matriz resultante
+
+                            for (int i = 0; i < 1; i++) {
+                                for (int j = 0; j < 1; j++) {
+                                    sextoTerR[i][j] = (m5[1][0]*m6[0][2])+(m5[1][1]*m6[1][2])+(m5[1][2]*m6[2][2]);
+                                }
+                            }
+
+                            //creación del septimo termino de la matriz resultante
+
+                            for (int i = 0; i < 1; i++) {
+                                for (int j = 0; j < 1; j++) {
+                                    septTerR[i][j] = (m5[2][0]*m6[0][0])+(m5[2][1]*m6[1][0])+(m5[2][2]*m6[2][0]);
+                                }
+                            }
+
+                            //creación del octavo termino de la matriz resultante
+
+                            for (int i = 0; i < 1; i++) {
+                                for (int j = 0; j < 1; j++) {
+                                    octaTerR[i][j] = (m5[2][0]*m6[0][1])+(m5[2][1]*m6[1][1])+(m5[2][2]*m6[2][1]);
+                                }
+                            }
+
+                            //creación del noveno termino de la matriz resultante
+
+                            for (int i = 0; i < 1; i++) {
+                                for (int j = 0; j < 1; j++) {
+                                    noveTerR[i][j] = (m5[2][0]*m6[0][2])+(m5[2][1]*m6[1][2])+(m5[2][2]*m6[2][2]);
+                                }
+                            }
+
+                            //------------------------------------------------------
+
+                            //impresion de cada termino de la matriz resultante
+
+                            System.out.println("El primer termino de la matriz es: ");
+                            for (int i=0; i < primerTerR.length; i++) {
+                                for (int j=0; j < primerTerR.length; j++) {
+                                    //System.out.print("[ "+ primerTerR[i][j] + " ]");
+                                }
+                                System.out.println(" ");
+                            }
+
+                            System.out.println("El segundo termino de la matriz es: ");
+                            for (int i=0; i < segundoTerR.length; i++) {
+                                for (int j=0; j < segundoTerR[0].length; j++) {
+                                    //System.out.print("[ "+ segundoTerR[i][j] + " ]");
+                                }
+                                System.out.println(" ");
+                            }
+
+                            System.out.println("El tercer termino de la matriz es: ");
+                            for (int i=0; i < tercerTerR.length; i++) {
+                                for (int j=0; j < tercerTerR[0].length; j++) {
+                                    System.out.print("[ "+ tercerTerR[i][j] + " ]");
+                                }
+                                System.out.println(" ");
+                            }
+
+                            System.out.println("El cuarto termino de la matriz es: ");
+                            for (int i=0; i < cuartoTerR.length; i++) {
+                                for (int j=0; j < cuartoTerR[0].length; j++) {
+                                    System.out.print("[ "+ cuartoTerR[i][j] + " ]");
+                                }
+                                System.out.println(" ");
+                            }
+
+                            System.out.println("El quinto termino de la matriz es: ");
+                            for (int i=0; i < quintoTerR.length; i++) {
+                                for (int j=0; j < quintoTerR[0].length; j++) {
+                                    System.out.print("[ "+ quintoTerR[i][j] + " ]");
+                                }
+                                System.out.println(" ");
+                            }
+
+                            System.out.println("El sexto termino de la matriz es: ");
+                            for (int i=0; i < sextoTerR.length; i++) {
+                                for (int j=0; j < sextoTerR[0].length; j++) {
+                                    System.out.print("[ "+ sextoTerR[i][j] + " ]");
+                                }
+                                System.out.println(" ");
+                            }
+
+                            System.out.println("El septimo termino de la matriz es: ");
+                            for (int i=0; i < septTerR.length; i++) {
+                                for (int j=0; j < septTerR[0].length; j++) {
+                                    System.out.print("[ "+ septTerR[i][j] + " ]");
+                                }
+                                System.out.println(" ");
+                            }
+
+                            System.out.println("El octavo termino de la matriz es: ");
+                            for (int i=0; i < octaTerR.length; i++) {
+                                for (int j=0; j < octaTerR[0].length; j++) {
+                                    System.out.print("[ "+ octaTerR[i][j] + " ]");
+                                }
+                                System.out.println(" ");
+                            }
+
+                            System.out.println("El noveno termino de la matriz es: ");
+                            for (int i=0; i < noveTerR.length; i++) {
+                                for (int j=0; j < noveTerR[0].length; j++) {
+                                    System.out.print("[ "+ noveTerR[i][j] + " ]");
+                                }
+                                System.out.println(" ");
+                            }
+
+                            //impresion de la matriz resultante
+
+                            System.out.println("Matriz Resultante R: ");
+
+                            matResultantes[0][0] = primerTerR[0][0];
+                            matResultantes[0][1] = segundoTerR[0][0];
+                            matResultantes[0][2] = tercerTerR[0][0];
+                            matResultantes[1][0] = cuartoTerR[0][0];
+                            matResultantes[1][1] = quintoTerR[0][0];
+                            matResultantes[1][2] = sextoTerR[0][0];
+                            matResultantes[2][0] = septTerR[0][0];
+                            matResultantes[2][1] = octaTerR[0][0];
+                            matResultantes[2][2] = noveTerR[0][0];
+
+                            for (int x=0; x < matResultantes.length; x++) {
+                                System.out.print("[");
+                                for (int y=0; y < matResultantes[x].length; y++) {
+                                    System.out.print (matResultantes[x][y]);
+                                    if (y!=matResultantes[x].length-1) System.out.print("\t");
+                                }
+                                System.out.println("]");
+                            }
+
+                            //--------------------------------------------------------
+                            
+                            break;
+
+                        }
+
+                    }
 
                         break;
                     }
